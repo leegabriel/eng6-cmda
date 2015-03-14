9320a79342f0948c2e9b907462b410962321287c
Gabriel Lee <lee.j.gabriel@gmail.com>
2015-03-14 01:09:11 -0700

    working data scraping. Need to fix months and pick better polynomial order

--- dataScrape.m
+++ dataScrape.m
@@ -1,9 +1,40 @@
 URL = 'http://online.wsj.com/mdc/public/page/2_3023-cashprices.html';
 str = urlread(URL);
 fileID = fopen('webdata.txt', 'w');
-% fprintf(fileID,'%s\r\n',str);
-aluminumIndex = regexp(str, 'Aluminum');
-copperIndex = regexp(str, 'Copper');
-cottonIndex = regexp(str, 'Cotton, 1');
-cocoaIndex = regexp(str, 'Cocoa');
+newStr = regexprep(str,'\s*',' '); 
+fprintf(fileID,'%s\r\n',str);
+aluminumIndex = regexp(newStr, 'Aluminum');
+copperIndex = regexp(newStr, 'Copper');
+cottonIndex = regexp(newStr, 'Cotton, 1');
+cocoaIndex = regexp(newStr, 'Cocoa');
 fclose(fileID);
+
+
+
+copperRow = newStr(copperIndex:copperIndex+300);
+expression = '<b>';
+startIndex = regexp(copperRow,expression);
+expression = '</b>';
+endIndex = regexp(copperRow, expression);
+copperRow(startIndex+3:endIndex-1)
+
+aluminumRow = newStr(aluminumIndex:aluminumIndex+300);
+expression = '<b>';
+startIndex = regexp(aluminumRow,expression);
+expression = '</b>';
+endIndex = regexp(aluminumRow, expression);
+aluminumRow(startIndex+3:endIndex-1)
+
+cottonRow = newStr(cottonIndex:cottonIndex+300);
+expression = '<b>';
+startIndex = regexp(cottonRow,expression);
+expression = '</b>';
+endIndex = regexp(cottonRow, expression);
+cottonRow(startIndex+3:endIndex-1)
+
+cocoaRow = newStr(cocoaIndex:cocoaIndex+300);
+expression = '<b>';
+startIndex = regexp(cocoaRow,expression);
+expression = '</b>';
+endIndex = regexp(cocoaRow, expression);
+cocoaRow(startIndex+3:endIndex-1)
