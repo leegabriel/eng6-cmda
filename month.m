9bfc181c18bd6a2696764ced4f0f856dcaf0e0dc
Gabriel Lee <lee.j.gabriel@gmail.com>
2015-03-15 02:41:26 -0700

    good night

--- month.m
+++ month.m
@@ -27,72 +27,80 @@ myStruct.decStarts = (12:12:r)';
 start = find(ismember(myStruct.years, choice1));
 stop = find(ismember(myStruct.years, choice2));
 
-commodity = 2;
-month = 'jan';
-if strcmp('jan', month)
-    january = project2Data(myStruct.janStarts(start:stop), commodity);
-    x = str2double(choice1):str2double(choice2);
+c = 2;
+m = 'jan';
+if strcmp('jan', m)
+    january = project2Data(myStruct.janStarts(start:stop), c);
+    x = start:stop;
     figure(1);
     plot(x, january);
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
+    set(gca,'xtick', cellstr(myStruct.years(start:stop)))
+elseif strcmp('feb', m)
+    february = project2Data(myStruct.febStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
     plot(x, february);
 
-elseif strcmp('mar', month)
-    march = project2Data(myStruct.marStarts(start:stop), commodity);
+elseif strcmp('mar', m)
+    march = project2Data(myStruct.marStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
     plot(x, march);
     
-elseif strcmp('apr', month)
-    april = project2Data(myStruct.aprStarts(start:stop), commodity);
+elseif strcmp('apr', m)
+    april = project2Data(myStruct.aprStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
     plot(x, april);
     
-elseif strcmp('may', month)
-    june = project2Data(myStruct.junStarts(start:stop), commodity);
+elseif strcmp('may', m)
+    may = project2Data(myStruct.mayStarts(start:stop), c);
+    x = str2double(choice1):str2double(choice2);
+    figure(1);
+    plot(x, may);
+    
+elseif strcmp('jun', m)
+    june = project2Data(myStruct.junStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
     plot(x, june);
-    
-elseif strcmp('jul', month)
-    july = project2Data(myStruct.julStarts(start:stop), commodity);
+elseif strcmp('jul', m)
+    july = project2Data(myStruct.julStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
-    plot(x, february);
+    plot(x, july);
     
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
+elseif strcmp('aug', m)
+    august = project2Data(myStruct.augStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
-    plot(x, february);
+    plot(x, august);
     
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
+elseif strcmp('sep', m)
+    september = project2Data(myStruct.sepStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
-    plot(x, february);
+    plot(x, september);
     
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
+elseif strcmp('oct', m)
+    october = project2Data(myStruct.octStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
-    plot(x, february);
+    plot(x, october);
     
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
+elseif strcmp('nov', m)
+    november = project2Data(myStruct.novStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
     figure(1);
-    plot(x, february);
+    plot(x, november);
     
-elseif strcmp('feb', month)
-    february = project2Data(myStruct.febStarts(start:stop), commodity);
-    x = str2double(choice1):str2double(choice2);
+elseif strcmp('dec', m)
+    december = project2Data(myStruct.decStarts(start:stop), c);
+    x = start:stop;
     figure(1);
-    plot(x, february);
+    plot(x, december);
+    set(gca, 'XTick', 1:stop-start+1, 'XTickLabel',myStruct.years(start:stop))
+    
 else
     fprintf('Error.\n');
 end
