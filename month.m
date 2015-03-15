bf71836d57677edd6c9aac4a81545e0d06d2a1fa
Gabriel Lee <lee.j.gabriel@gmail.com>
2015-03-15 16:38:40 -0700

    working monthly

--- month.m
+++ month.m
@@ -24,26 +24,25 @@ myStruct.octStarts = (10:12:r)';
 myStruct.novStarts = (11:12:r)';
 myStruct.decStarts = (12:12:r)';
 
-start = find(ismember(myStruct.years, choice1));
-stop = find(ismember(myStruct.years, choice2));
+start = find(ismember(myStruct.years, {choice1}));
+stop = find(ismember(myStruct.years, {choice2}));
 
 c = 2;
 m = getappdata(0, 'm')
 if strcmp('January', m)
     january = project2Data(myStruct.janStarts(start:stop), c);
     x = (str2double(choice1):str2double(choice2))';
-    figure(1);
+    figure;
     plot(datenum(x,1,1), january);
     dateFormat = 11;
-    datetick('x',dateFormat)
-    ylabel('Price')
+    datetick('x',dateFormat);
+    ylabel('Price');
     xlabel('Year');
-    title('January Prices from '); %, str2double(choice1), ' to ', ...
-        %xstr2double(choice2)));
+    title(horzcat('January Prices from ', choice1, ' to ', choice2));
 elseif strcmp('February', m)
     february = project2Data(myStruct.febStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), february);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -52,7 +51,7 @@ elseif strcmp('February', m)
 elseif strcmp('March', m)
     march = project2Data(myStruct.marStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), march);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -61,7 +60,7 @@ elseif strcmp('March', m)
 elseif strcmp('Apr', m)
     april = project2Data(myStruct.aprStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), april);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -70,7 +69,7 @@ elseif strcmp('Apr', m)
 elseif strcmp('May', m)
     may = project2Data(myStruct.mayStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), may);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -79,7 +78,7 @@ elseif strcmp('May', m)
 elseif strcmp('June', m)
     june = project2Data(myStruct.junStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), june);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -88,7 +87,7 @@ elseif strcmp('June', m)
 elseif strcmp('July', m)
     july = project2Data(myStruct.julStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), july);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -97,7 +96,7 @@ elseif strcmp('July', m)
 elseif strcmp('August', m)
     august = project2Data(myStruct.augStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), august);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -106,7 +105,7 @@ elseif strcmp('August', m)
 elseif strcmp('September', m)
     september = project2Data(myStruct.sepStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), september);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -115,7 +114,7 @@ elseif strcmp('September', m)
 elseif strcmp('October', m)
     october = project2Data(myStruct.octStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), october);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -124,7 +123,7 @@ elseif strcmp('October', m)
 elseif strcmp('November', m)
     november = project2Data(myStruct.novStarts(start:stop), c);
     x = str2double(choice1):str2double(choice2);
-    figure(1);
+    figure;
     plot(datenum(x,1,1), november);
     dateFormat = 11;
     datetick('x',dateFormat)
@@ -133,7 +132,7 @@ elseif strcmp('November', m)
 elseif strcmp('December', m)
     december = project2Data(myStruct.decStarts(start:stop), c);
     x = start:stop;
-    figure(1);
+    figure;
     plot(datenum(x,1,1), december);
     dateFormat = 11;
     datetick('x',dateFormat)
